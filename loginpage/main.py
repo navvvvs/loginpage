be6f6520933aca84f516b9bb9ac810bce0211e0e
Nia806 <nihalikarajsharma@gmail.com>
2024-11-24 20:04:05 +0530

    css dikhh rha h maateee

--- loginpage/main.py
+++ loginpage/main.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request, redirect, session, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
